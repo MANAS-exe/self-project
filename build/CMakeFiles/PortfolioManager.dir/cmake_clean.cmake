f5aaeca4f1c9fe378f4a501f70e072e0b970f261
MANAS-exe <131545414+MANAS-exe@users.noreply.github.com>
2025-09-30 22:40:06 +0530

    Add files via upload

---
+++ build/CMakeFiles/PortfolioManager.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PortfolioManager.dir/src/ApiClient.cpp.o"
+  "CMakeFiles/PortfolioManager.dir/src/ApiClient.cpp.o.d"
+  "CMakeFiles/PortfolioManager.dir/src/CLI.cpp.o"
+  "CMakeFiles/PortfolioManager.dir/src/CLI.cpp.o.d"
+  "CMakeFiles/PortfolioManager.dir/src/Database.cpp.o"
+  "CMakeFiles/PortfolioManager.dir/src/Database.cpp.o.d"
+  "CMakeFiles/PortfolioManager.dir/src/Portfolio.cpp.o"
+  "CMakeFiles/PortfolioManager.dir/src/Portfolio.cpp.o.d"
+  "CMakeFiles/PortfolioManager.dir/src/main.cpp.o"
+  "CMakeFiles/PortfolioManager.dir/src/main.cpp.o.d"
+  "PortfolioManager.exe"
+  "PortfolioManager.pdb"
+  "libPortfolioManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PortfolioManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
